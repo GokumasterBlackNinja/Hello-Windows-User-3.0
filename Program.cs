3821f9f0a751c496dfaa9057c129a142e612d2a5
GokumasterBlackNinja <132934846+GokumasterBlackNinja@users.noreply.github.com>
2023-05-16 07:50:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MorningGreetings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Windows User!");
+            Console.WriteLine("It's a new day with new possibilities.");
+            Console.WriteLine("Just smile and do it!");
+            Console.WriteLine("Remember, others have it worse.");
+            Console.WriteLine("Appreciate your legs, hands, and eyes,");
+            Console.WriteLine("and don't complain, even to the women during their period.");
+            Console.WriteLine("Grab your coffee and car keys and be like Forrest Gump.");
+            Console.WriteLine("Never stop running to achieve your goals.");
+            Console.WriteLine("Keep calm and don't worry about anything.");
+            Console.WriteLine("Remember, it can always be worse.");
+            Console.WriteLine("Have a nice day, hero!");
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
